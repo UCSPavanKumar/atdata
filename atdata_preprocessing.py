385a5dbef4a35e9599fa5dc80a05565747008cc7
Upadrasta Chandra Sekhar Pavan Kumar <pavanupadrasta@gmail.com>
2024-09-26 09:49:48 +0530

    Update atdata_preprocessing.py

--- atdata_preprocessing.py
+++ atdata_preprocessing.py
@@ -1,11 +1,9 @@
-from pyspark.sql.window import Window
 from pyspark.sql import Window,SparkSession
 from pyspark.sql.functions import when, col, concat_ws, upper, max, md5, rank, row_number, collect_list, sort_array
 from pyspark.sql import functions as F
 from pyspark.sql.functions import col, count, lit
-S3_INPUT_DATA_PATH  = 's3://ascend-oeidp-batch-pinning-input-074628058490-us-east-1/AtData/supertrace_atdata/atdata_rawfiles/experian_ad_fullbuild_202407_01.txt.gz'
-S3_OUTPUT_DATA_PATH = ''
-def main():
+import sys
+def main(S3_INPUT_DATA_PATH,S3_OUTPUT_DATA_PATH):
     spark                   = SparkSession.builder.appName("atdata_pre_process").getOrCreate()
     
     df_limit                =   spark.read.format("csv").\
@@ -16,6 +14,7 @@ def main():
     new_fieldnames_fields   = ["email", "first_name", "last_name", "address1", "address2", "city", "state", "zip5", "zip4", "website", "ip", "opt_in_date","email_status_id"]
 
     df                      = df_limit.toDF(*new_fieldnames_fields)
+				df																						= df.limit(3500)
     df                      = df.dropna(how = 'email')
     valid_df                = df.filter("email_status_id = 1")
     hash_cols               =  ['first_name', 'last_name', 'address1', 'city', 'state', 'zip5', 'zip4']
@@ -31,7 +30,9 @@ def main():
                     lit("1")    
                     )
     df = valid_df.withColumn("record_sequence_no", row_number().over(windowSpec)).withColumn("record_seq_no", col("record_sequence_no")).withColumn("full_zip",concat_ws(col("zip5"), col("zip4")))
-
+				df.write.parquet(S3_OUTPUT_DATA_PATH)
 
 if __name__ == '__main__':
+				S3_INPUT_DATA_PATH = sys.argv[1]
+				S3_OUTPUT_DATA_PATh =  sys.argv[2]
     main()
